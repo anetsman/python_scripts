63a308e9f5fd1b4c34ec8cf7988d4ae45e8d491b
Alexander <sawka_ne@mail.ru>
2013-03-04 18:37:17 +0200

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -1,5 +1,5 @@
 degreesC, degreesF = 0, 0
-degreesF = int(input('Enter the temperature in degrees F: '))
+degreesF = eval(input('Enter the temperature in degrees F: '))
 #x = degreesF
 degreesC = 5/9*(degreesF - 32)
 print(degreesF, 'degrees F =', degreesC, 'degrees C' )
