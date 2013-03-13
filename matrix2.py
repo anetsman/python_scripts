b342596dceba5c0027664c4f48bb66482bcf1183
Alexander <sawka_ne@mail.ru>
2013-03-13 13:02:57 +0200

    1

---
+++ matrix2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from random import randrange
+
+def mc():
+	li = []
+	column = eval(input("enter number of columns: "))
+	row = eval(input("enter number of rows: "))
+	for c in range (1, row + 1):
+		l = list()
+		a = 0
+		while a < column:
+			b = randrange(1, 1000)
+			l += [b]
+			a += 1
+		print(l)
+		li += l
+	return li
+li = mc()
+listm = []
+print(li)
+k = 0
+n = 1
+while n < len(li):
+	while k < (len(li)-n):
+		if li[k] > li[k+1]:
+			li[k], li[k+1] = li[k+1], li[k]
+		k += 1
+	k = 0
+	n += 1
+print(li)
