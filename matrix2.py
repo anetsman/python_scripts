bc97d985bdc95daf464bd07677b16a73e8aa782a
Alexander <sawka_ne@mail.ru>
2013-03-13 15:37:37 +0200

    1

--- matrix2.py
+++ matrix2.py
@@ -15,17 +15,20 @@ def mc():
 			a += 1
 		print(l)
 		li += l
-	li = [li[i:i + row] for i in range (0, len(li), row)	
+	print(li)	
+	k = 0
+	n = 1
+	while n < len(li):
+		while k < (len(li)-n):
+			if li[k] > li[k+1]:
+				li[k], li[k+1] = li[k+1], li[k]
+			k += 1
+		k = 0
+		n += 1
+	def dlist():
+		return [li[i:i + row] for i in range (0, len(li), row)
+	li = dlist()
 	return li
 li = mc()
 print(li)
-k = 0
-n = 1
-while n < len(li):
-	while k < (len(li)-n):
-		if li[k] > li[k+1]:
-			li[k], li[k+1] = li[k+1], li[k]
-		k += 1
-	k = 0
-	n += 1
-print(li)
+
