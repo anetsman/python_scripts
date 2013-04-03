8a9004c2e469032234003c64d0cc070a77b33da0
GENius <genius@GENius-LT.(none)>
2013-04-03 22:07:26 +0300

    refactored ./Lviv_task.py

--- Lviv_task.py
+++ Lviv_task.py
@@ -1,43 +1,49 @@
-#! /usr/bin/python
+#!/usr/bin/python
 
 import unittest
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-#import sys
+import sys
+#print(sys.path)
+#sys.path.append("/usr/lib/python2.7/dist-packages/")
+#sys.path.append("/usr/lib/python2.7/dist-packages/scapy/")
+#print(sys.path)
 from scapy.all import *
 
 host = "127.0.0.1"
 number = int(input("Enter number of pings: "))
 
-class TestPing(unittest.TestCase):
-	def pingICMP(host, number):
+class TestPing:
+	def pingICMP(self,host, number):
 		i = 0
-		p = (IP(dst='google.com')/ICMP())
+		p = (IP(dst=host)/ICMP())
 		p.show()
 		while i < number:
 			response = sr1(p)
 #			response.show()
 			i += 1
-	pingICMP(host, number)
-
+#	pingICMP(host, number)
+'''
 	def pingUDP(host, number):
-       	        i = 0
-               	p = (IP(dst=host)/UDP())
-               	p.show()
-           	while i < number:
-                       	response = sr1(p)
-#                      	response.show()
-                       	i += 1
-	pingUDP(host, number)
+	   	i = 0
+		p = (IP(dst=host)/UDP())
+			   	p.show()
+		while i < number:
+				response = sr1(p)
+#					  	response.show()
+ 				i += 1
+#	pingUDP(host, number)
 
-        def pingHTTP(host, number):
-                i = 0
-                p = (IP(dst=host)/'GET / HTTP/1.1\r\n\r\n')
-                p.show()
-                while i < number:
-                        response = sr1(p)
-#                       response.show()
-                        i += 1
-        pingHTTP(host, number)
-
+		def pingHTTP(host, number):
+				i = 0
+				p = (IP(dst=host)/'GET / HTTP/1.1\r\n\r\n')
+				p.show()
+				while i < number:
+					response = sr1(p)
+#					   response.show()
+					i += 1
+#		pingHTTP(host, number)
+'''
+pinger = TestPing()
+pinger.pingICMP(host,number)
